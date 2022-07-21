5a32cefacda82aa5083884291dbd9a5786c0e311
anuzza <anuja3580@gmail.com>
2022-07-21 04:52:14 -0500

    array & objects

---
+++ basics.ts
@@ -0,0 +1,31 @@
+//Primitives: number, string, boolean
+//more complex types: arrays. objects
+//function types, parameters
+
+//Primitives
+let age: number;
+age = 25;
+let username: string;
+username = "anuzza";
+let isStudent: boolean;
+isStudent = true;
+
+//more complex types
+let hobbies: string[];
+
+hobbies = ["sports", "music", "cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+//array of objects
+let people: {
+  name: string;
+  age: number;
+}[];
